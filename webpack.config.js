a80ade33a79e09e1fc4c2a50c86cd8419ea669c0
mattand <mjande@umich.edu>
2022-11-03 16:16:35 -0400

    Init ESLint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'The Weather App',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
